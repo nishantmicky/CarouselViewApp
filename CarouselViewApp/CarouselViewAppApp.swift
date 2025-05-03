2648d53884a532bf9879331934a804a48c296006
Nishant Kumar <nishantkumar@Nishants-MacBook-Pro.local>
2025-05-03 13:59:24 +0530

    Initial commit

---
+++ CarouselViewApp/CarouselViewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselViewAppApp.swift
+//  CarouselViewApp
+//
+//  Created by Nishant Kumar on 03/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CarouselViewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
